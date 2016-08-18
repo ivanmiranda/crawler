595cf196b3a25f4e8f374f450ce7c01deba2e951
Ivan Miranda <pa.ivan.miranda@gmail.com>
2016-08-18 11:05:28 -0500

    Avances20160818

--- amazon.php
+++ amazon.php
@@ -17,7 +17,7 @@ function getContent($url) {
 
 function processBook($url) {
 	$contentBook = getContent($url);
-	echo 'BOOK::' . $url . '::' . strlen(trim($contentBook)) . "\n";
+	echo 'BOOK::' . html_entity_decode($url) . '::' . strlen(trim($contentBook)) . "\n";
 	if ($details = str_get_html($contentBook)) {
 		$book = [];
 		foreach($details->find('span[id=productTitle]') as $data) {
