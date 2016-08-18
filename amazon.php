080940a07cab1c156edc3e212f2219354c5eedd7
Ivan Miranda <pa.ivan.miranda@gmail.com>
2016-08-17 19:00:56 -0500

    Bug

--- amazon.php
+++ amazon.php
@@ -17,13 +17,11 @@ function getContent($url) {
 
 function process($url) {
 	if($dom = str_get_html(getContent($url))) {
-		if (PHP_SAPI == "cli") $lb = "\n"; 
-		else $lb = "<br />"; 
-		//echo "--> {$url}".$lb; 
 	    foreach ($dom->find('div[class=s-item-container]') as $item) {
 	      	$autor = '';
 	      	foreach ($item->find('a[class=a-link-normal s-access-detail-page  a-text-normal]') as $url) {
-	      		if($details = str_get_html(getContent($url->attr['href']))) {
+	      		$details = str_get_html(getContent($url->attr['href']))
+	      		if(trim($details != '')) {
 		      		$book = [];
 					foreach($details->find('span[id=productTitle]') as $data) {
 						$book['title'] = trim(html_entity_decode($data->innertext));
