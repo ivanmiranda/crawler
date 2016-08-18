38e567bd60e8cf6e087b34df305bdd2768a41e8a
Ivan Miranda <pa.ivan.miranda@gmail.com>
2016-08-18 10:32:01 -0500

    Avances20160818

--- amazon.php
+++ amazon.php
@@ -6,7 +6,7 @@ include("./simple_html_dom.php");
 
 function getContent($url) {
 	$ch = curl_init();
-	$timeout = 50;
+	$timeout = 10;
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
